c4d18491b08cbc746085a6cd6021aad10e4db9fb
emranblue <emran.blue.120@gmail.com>
2025-08-10 17:31:03 +0600

    feat: Add dark theme

--- main.cpp
+++ main.cpp
@@ -4,23 +4,35 @@
 #include <QSettings>
 #include <QFont>
 #include <QPalette>
+#include <QStyleFactory>
 #include "pdfextractiondialog.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    app.setStyle("Fusion");
+    app.setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    app.setPalette(darkPalette);
 
     QSettings settings("MyCompany", "PdfExtractor");
     int fontSize = settings.value("appFontSize", 12).toInt();
-    QColor themeColor = settings.value("themeColor", QColor(Qt::white)).value<QColor>();
-
+    
     QFont font = app.font();
     font.setPointSize(fontSize);
     app.setFont(font);
-
-    QPalette palette = app.palette();
-    palette.setColor(QPalette::Window, themeColor);
-    app.setPalette(palette);
 
     app.setWindowIcon(QIcon(":/icons/app_icon.png")); // Set application icon
     QDir::setCurrent("/media/emran/sdcard/bcs");
